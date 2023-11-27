ff5a8f302f7f37aa7ee037673f26ecb367eb8ffc
Jose Jordan <mundodigital.pro@gmail.com>
2023-11-27 12:01:59 +0100

    Better UI on GameItem

--- app/src/main/java/com/example/mygameapp/MainActivity.kt
+++ app/src/main/java/com/example/mygameapp/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.BottomNavigation
@@ -20,6 +22,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -28,23 +32,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.mygameapp.ui.theme.MyGameAppTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyGameAppTheme {
-/*                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-
-                }*/
                 MyApp {
                     MyScreenContent()
                 }
@@ -52,24 +52,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-/*
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    MyGameAppTheme {
-        Greeting("Android")
-    }
-}
-*/
 
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
@@ -132,11 +114,29 @@ fun MyScreenContent() {
 }
 
 
+/*
 @Composable
 fun HeaderSection(title: String) {
     // Componente para el encabezado de cada sección
     Text(text = title)
 }
+*/
+
+@Composable
+fun HeaderSection(title: String) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary, // Usa el color primario del tema para el fondo
+        modifier = Modifier.fillMaxWidth() // El encabezado debe ocupar todo el ancho
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineMedium, // Usa un estilo más grande para el encabezado
+            color = MaterialTheme.colorScheme.onPrimary, // El color del texto debe contrastar con el fondo
+            modifier = Modifier.padding(8.dp) // Agrega espaciado alrededor del texto del encabezado
+        )
+    }
+}
+
 
 @Composable
 fun GameList(games: List<Game>) {
@@ -148,7 +148,7 @@ fun GameList(games: List<Game>) {
     }
 }
 
-@Composable
+/*@Composable
 fun GameItem(game: Game) {
     Card {
         Column {
@@ -168,12 +168,71 @@ fun GameItem(game: Game) {
             }
         }
     }
+}*/
+
+@Composable
+fun GameItem(game: Game) {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp) // Usa CardDefaults para Material3
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) { // Usa Row para imagen a la izquierda y texto a la derecha
+            Image(
+                painter = rememberImagePainter(game.imageUrl),
+                contentDescription = "Game Image",
+                modifier = Modifier.size(100.dp) // Establece un tamaño fijo para las imágenes
+            )
+            Column(modifier = Modifier.padding(8.dp)) { // Agrega espacio dentro de la columna
+                Text(
+                    text = game.title,
+                    style = MaterialTheme.typography.headlineMedium, // Usa un estilo más prominente para el título
+                    fontWeight = FontWeight.Bold // Hace el título en negrita
+                )
+                RatingBar(rating = game.rating) // Crea una barra de calificación con estrellas
+                Text(text = "Release Date: ${game.releaseDate}")
+                // Muestra los géneros en chips
+                Row {
+                    game.genre.forEach { genre ->
+                        Chip(label = genre)
+                    }
+                }
+            }
+        }
+    }
+}
+@Composable
+fun RatingBar(rating: Float) {
+    Row {
+        repeat((rating.roundToInt())) { // Muestra estrellas llenas basadas en la calificación
+            Icon(
+                Icons.Filled.Star,
+                contentDescription = "Star",
+                tint = Color.Yellow // Usa el color amarillo para las estrellas
+            )
+        }
+    }
 }
 
+/*
 @Composable
 fun Chip(label: String) {
     // Puedes diseñar este componente para que se vea como un chip de género de juego
     Text(text = label)
+}*/
+
+@Composable
+fun Chip(label: String) {
+    Surface(
+        shape = MaterialTheme.shapes.small, // Usa una forma definida en tu tema
+        color = MaterialTheme.colorScheme.secondary, // Usa el color secundario del tema
+        modifier = Modifier.padding(4.dp) // Agrega espaciado alrededor del chip
+    ) {
+        Text(
+            text = label,
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp), // Espaciado interno para el texto del chip
+            color = MaterialTheme.colorScheme.onSecondary // El color del texto debe contrastar con el fondo del chip
+        )
+    }
 }
 
 
